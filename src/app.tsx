fc42593734bcda99811bfb72be6872b728ed2a45
gbagul <gbagul@avaya.com>
2022-07-07 20:17:37 +0530

    add code

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
-	const name = "gaurav"
+	const name = "gaurav";
 	return (
-		<>
-			<h1>
-				Hello World edited- {name}</h1>
-			<ClickCounter />
+	<>
+			<h1>Hello World edited-
+				{name}</h1>
+					<ClickCounter />
 		</>
 	);
 };
