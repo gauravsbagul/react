62ea04e96611d7254a1885cf922db6a0fb79318d
gbagul <gbagul@avaya.com>
2022-07-07 20:11:58 +0530

    Check linting errors

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
+	const name = "gaurav"
 	return (
 		<>
-			<h1>Hello World edited</h1>
+			<h1>
+				Hello World edited</h1>
 			<ClickCounter />
 		</>
 	);
